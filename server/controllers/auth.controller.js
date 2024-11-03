42dc049d59c01e1152212426edd565f2dc599357
RedInkForest <Pradhan751957@gmail.com>
2024-11-02 21:18:28 -0500

    setup

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,20 +1,7 @@
-<<<<<<< Updated upstream
-
-import jwt from 'jsonwebtoken';
-
-export const signUp = async (req, res) => {
-    try {
-
-    }
-    catch (error){
-        
-    }
-=======
 import {tokenGenerate} from '../utils/tokenGenerate.js';
 import User from '../models/user.model.js';
 import bycrypt from 'bcryptjs';
 
 export const signup = async (req, res) => {
 
->>>>>>> Stashed changes
 };
